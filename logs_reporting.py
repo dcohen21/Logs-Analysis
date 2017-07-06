cd2701627bc6e4ef7651d626688ff1b7af10bc29
dcohen21 <david.cohen33@gmail.com>
2017-07-06 11:15:58 -0400

    Most popular authors finished and error days in progress

--- logs_reporting.py
+++ logs_reporting.py
@@ -12,15 +12,71 @@ def popular_articles():
     connection = psycopg2.connect(database=DB_NAME)
     cursor = connection.cursor()
     cursor.execute(
-        """select articles.title, count(log.path) from articles, log 
+        """select articles.title, count(log.path) 
+        from articles, log 
         where log.path like '%'||articles.slug 
         group by articles.title 
-        order by count(log.path) desc limit 3;""")
+        order by count(log.path) desc limit 3;"""
+    )
     results = cursor.fetchall()
-    print('\n' + "Most popular three articles of all time:" + '\n')
+    print('\n\n' + "Most popular three articles of all time:" + '\n')
     for item in results:
         print("\"" + item[0].title() + "\": " + str(item[1]) + " views")
+    connection.close()
+
+
+def popular_authors():
+    """List authors in order of popularity based on total article views"""
+
+    connection = psycopg2.connect(database=DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute(
+        """create view author_slug as 
+        select authors.name, articles.slug 
+        from articles, authors 
+        where articles.author = authors.id;"""
+    )
+    cursor.execute(
+        """select author_slug.name, count(log.path) 
+        from author_slug, log 
+        where log.path like '%'||author_slug.slug 
+        group by author_slug.name 
+        order by count(log.path) desc;"""
+    )
+    results = cursor.fetchall()
+    print('\n\n' + "Authors listed by popularity as defined by total article views:" + '\n')
+    for item in results:
+        print(item[0] + ": " + str(item[1]) + " views")
+    connection.close()
+
+
+def error_days():
+    connection = psycopg2.connect(database=DB_NAME)
+    cursor = connection.cursor()
+    cursor.execute(
+        """create view error_view as 
+        select count(*)::numeric as num, time::date as day 
+        from log where status != '200 OK' 
+        group by day order by day desc;"""
+    )
+    cursor.execute(
+        """create view total_view as 
+        select count(*)::numeric as num, time::date as day 
+        from log group by day;"""
+    )
+    cursor.execute(
+        """select *, (error_view.num/total_view.num)*100 as pct 
+        from total_view 
+        join error_view on total_view.day=error_view.day 
+        where (error_view.num/total_view.num)*100 > 1;"""
+    )
+    results = cursor.fetchall()
+    print('\n\n' + "Days in which 404 errors accounted for >1% of requests:" + '\n')
+    for item in results:
+        print(str(item[3]) + ": " + str(item[4]))
 
 
 if __name__ == "__main__":
     popular_articles()
+    popular_authors()
+    error_days()
